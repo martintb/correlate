69de923a6928046180b68839a8486ecc96db76cb
Tyler Martin <martintb@udel.edu>
2016-05-04 21:36:22 -0400

    Updated Valet Scripts
    This code now depends on gcc>4.9 so the valet scripts had to be updated

---
+++ env/Valet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+vpkg_require gcc/4.9.3
+vpkg_require openmpi/1.10.2-gcc-4.9.3
+vpkg_devrequire jlab-boost/gcc4.9
+
+
--- env/valet.sh
+++ env/valet.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
 
-vpkg_require python gcc/4.8.3
+vpkg_require gcc/4.8.3
 vpkg_require openmpi/1.8.2-gcc-4.8.3
-vpkg_devrequire jlab-rapidxml jlab-boost 
-vpkg_devrequire lapack/3.5.0
-vpkg_devrequire atlas/3.10.2
-vpkg_devrequire jlab-arma
-vpkg_require python-matplotlib typy/3
+vpkg_devrequire jlab-boost 
+# vpkg_devrequire jlab-rapidxml
+# vpkg_devrequire lapack/3.5.0
+# vpkg_devrequire atlas/3.10.2
+# vpkg_devrequire jlab-arma
+# vpkg_require python python-matplotlib typy/3
