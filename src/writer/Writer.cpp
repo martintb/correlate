e2454eebeea858b3cf6a49dedd2b641dad6c9cbf
Tyler Martin <martintb@udel.edu>
2016-05-13 16:07:22 -0400

    bugfixes

--- src/writer/Writer.cpp
+++ src/writer/Writer.cpp
@@ -5,8 +5,9 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <chrono>
-#include <ctime>
+#include <chrono> // now()
+#include <ctime>  //ctime()
+#include <cmath> //PI
 
 #include "debug.hpp"
 #include "floatWriter.hpp"
@@ -38,37 +39,6 @@ Writer::ptr Writer::get(Config::ptr &conf) {
   }
 }
 
-void Writer::buildCoeff() {
-  float tot_natoms;
-  if (conf->selfHist) {
-    tot_natoms = conf->natoms1;
-  } else {
-    tot_natoms = conf->natoms1+conf->natoms2;
-  }
-
-  // process incremental box data
-  float lx = box[0]/step_count;
-  float ly = box[1]/step_count;
-  float lz = box[2]/step_count;
-  float box_volume = lx*ly*lz;
-  float pair_rho = pair_count_master/box_volume/step_count;
-
-  coeffAdd.assign(conf->xsize,0.0f);
-  coeffMult.assign(conf->xsize,1.0f);
-  for (int i=0;i<conf->xsize;i++) {
-    float x1 = i*conf->dx;
-    float x2 = (i+1)*conf->dx;
-    float vol = (4.0/3.0) * (3.14159) * (x2*x2*x2  - x1*x1*x1);
-    if (conf->kernel == Config::rdf) 
-    {
-      coeffMult[i]*=1.0/(step_count*vol*pair_rho);
-    } 
-    else if ( conf->kernel == Config::omega) {
-      if (conf->selfHist) coeffAdd[i] += 1.0;
-      coeffMult[i]*=1.0/(step_count*tot_natoms*x2);
-    }
-  }
-}
 
 void Writer::write(bool vertical,bool reset) {
   if (not vertical) {
@@ -115,43 +85,6 @@ void Writer::writeVertical(bool reset) {
     this->reset(); //reset counters and arrays
 }
 
-void Writer::writeHorizontal(bool reset) {
-  gather(); // fill vecMaster with data from all procs
-  if (conf->isRoot()) {
-    buildCoeff();
-
-    float cutoff;
-    if ( conf->kernel == Config::histogram or conf->kernel == Config::rdf) {
-      cutoff = box[0]/(step_count*2.0);
-      cout << "--> Cutting off data at lx/2.0 = "  << cutoff << endl;
-    } else {
-      cutoff = conf->xmax;
-      cout << "--> Using full data range with xmax = "  << cutoff << endl;
-    }
-
-    if (not file_opened) {
-      if (not (conf->output_file->exists())) { 
-        writeHeader(cutoff); 
-      } else if ((conf->overwrite) and (conf->output_file->exists())) { 
-        writeHeader(cutoff); 
-      }
-      file_opened = true;
-    }
-    ofstream file(conf->output_file->path.string(),std::ofstream::app);
-    for (int i=0;i<conf->xsize;i++) {
-      float x = i*conf->dx;
-      if (x<cutoff) {
-        file << vecMaster[i]*coeffMult[i] + coeffAdd[i] << " ";
-      }
-    }
-    file << endl;
-    file.close();
-  }
-  MPI::COMM_WORLD.Barrier();
-
-  if (reset)
-    this->reset(); //reset counters and arrays
-}
 
 void Writer::writeHeader(int cutoff) {
   ofstream file(conf->output_file->path.string());
@@ -167,7 +100,6 @@ void Writer::writeHeader(int cutoff) {
   int ompi_major = OMPI_MAJOR_VERSION;
   int ompi_minor = OMPI_MINOR_VERSION;
   int ompi_patch = OMPI_RELEASE_VERSION;
-
 
   file << "# --------------------------------------------------------------------------" << endl;
   file << "#                             >>> CORRELATE <<<" << endl;
@@ -210,4 +142,76 @@ void Writer::writeHeader(int cutoff) {
     }
   }
   file << endl;
+}
+
+void Writer::writeHorizontal(bool reset) {
+  gather(); // fill vecMaster with data from all procs
+  if (conf->isRoot()) {
+    buildCoeff();
+
+    float cutoff;
+    if ( conf->kernel == Config::histogram or conf->kernel == Config::rdf) {
+      cutoff = box[0]/(step_count*2.0);
+      cout << "--> Cutting off data at lx/2.0 = "  << cutoff << endl;
+    } else {
+      cutoff = conf->xmax;
+      cout << "--> Using full data range with xmax = "  << cutoff << endl;
+    }
+
+    if (not file_opened) {
+      if (not (conf->output_file->exists())) { 
+        writeHeader(cutoff); 
+      } else if ((conf->overwrite) and (conf->output_file->exists())) { 
+        writeHeader(cutoff); 
+      }
+      file_opened = true;
+    }
+    ofstream file(conf->output_file->path.string(),std::ofstream::app);
+    for (int i=0;i<conf->xsize;i++) {
+      float x     = (i*conf->dx);
+      float value = (vecMaster[i]*coeffMult[i] + coeffAdd[i]);
+      if (x<cutoff) {
+        file << value << " ";
+      }
+    }
+    file << endl;
+    file.close();
+  }
+  MPI::COMM_WORLD.Barrier();
+
+  if (reset)
+    this->reset(); //reset counters and arrays
+}
+
+void Writer::buildCoeff() {
+  float tot_natoms;
+  if (conf->selfHist) {
+    tot_natoms = conf->natoms1;
+  } else {
+    tot_natoms = conf->natoms1+conf->natoms2;
+  }
+
+  // process incremental box data
+  pair_count_master /= step_count;
+  float lx = box[0]/step_count;
+  float ly = box[1]/step_count;
+  float lz = box[2]/step_count;
+  float box_volume = lx*ly*lz;
+  float pair_rho = pair_count_master/box_volume;
+
+  coeffAdd.assign(conf->xsize,0.0f);
+  coeffMult.assign(conf->xsize,1.0f);
+  for (int i=0;i<conf->xsize;i++) {
+    float x1  = (i*conf->dx);
+    float x2  = ((i+1)*conf->dx);
+    float vol = ((4.0/3.0) * M_PI * (x2*x2*x2  - x1*x1*x1));
+    if (conf->kernel == Config::rdf) 
+    {
+      coeffMult[i]*=(1.0/(step_count*vol*pair_rho));
+    } 
+    else if ( conf->kernel == Config::omega) {
+      if (conf->selfHist) coeffAdd[i] += 1.0;
+      coeffMult[i]*=(1.0/(step_count*tot_natoms*x2));
+    }
+  }
 }
--- src/writer/floatWriter.cpp
+++ src/writer/floatWriter.cpp
@@ -20,7 +20,7 @@ void floatWriter::gather() {
                        MPI::FLOAT,MPI::SUM,0);
   vecMaster.assign(allVecFloat.begin(),allVecFloat.end());
 
-  pair_count_master=0;
+  pair_count_master = 0;
   MPI::COMM_WORLD.Reduce(&pair_count,&pair_count_master,1, MPI::UNSIGNED_LONG,MPI::SUM,0);
 }
 
